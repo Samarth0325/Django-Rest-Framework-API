b9cf32c15fd35f307b88a1b2a360787d5f6f01fb
Samarth0325 <samarthdeshmukhe3@gmail.com>
2026-02-19 18:38:05 +0530

    Initial Django REST Framework project

---
+++ quickstart/views.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import Group, User
+from rest_framework import permissions, viewsets
+from quickstart.serializers import GroupSerializer, UserSerializer
+
+# Create your views here.
+
+
+class UserViewset(viewsets.ModelViewSet):
+    #API endpoint that allow user to be viewed or edited.
+    
+    queryset = User.objects.all() #Date_Joined
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class GroupViewset(viewsets.ModelViewSet):
+    #API endpoint that allow groups to be viewed or edited.
+    
+    queryset = User.objects.all()   #Date_Joined
+    serializer_class = GroupSerializer
+    permission_classes = [permissions.IsAuthenticated]    
